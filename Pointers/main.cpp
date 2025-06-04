ed7d47425bce7e8ac2d0bb5c272470f86ec70a54
ilyav <ilyav@ILIA>
2025-06-04 23:06:22 +0500

    Add project files.

---
+++ Pointers/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+//#define Pointers_basics
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef Pointers_basics
+	int a = 2;
+	int* pa = &a;
+	cout << a << endl;
+	cout << &a << endl;
+	cout << pa << endl;
+	cout << *pa << endl;
+
+	int* pb;
+	int b = 3;
+	pb = &b;
+	cout << pb << endl;
+	cout << *pb << endl;
+#endif
+
+	const int n = 5;
+	short arr[n] = { 3,5,8,13,21 };
+	cout << arr << endl;
+	cout << *arr << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << *(arr + i) << "\t";
+	}
+	cout << endl;
+}
