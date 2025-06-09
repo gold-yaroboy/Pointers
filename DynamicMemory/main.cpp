e5c658083a779dd4777e5abec0a2ba94384e4720
ilyav <ilyav@ILIA>
2025-06-09 23:25:54 +0500

    TODO: insert(), erase().

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
 
+int* push_back(int arr[], int& n, const int value);
+int* push_front(int arr[], int& n, const int value);
+
+int* pop_back(int arr[], int& n);
+int* pop_front(int arr[], int& n);
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -11,8 +17,24 @@ void main()
 	cout << "go size array: "; cin >> n;
 	int* arr = new int[n];
 	FillRand(arr, n);
-	Print(arr, n);	
-	delete[]arr;
+	Print(arr, n);
+
+	int value;
+	cout << "go push_back value: "; cin >> value;
+	arr = push_back(arr, n, value);
+	Print(arr, n);
+
+	cout << "go push_front value: "; cin >> value;
+	arr = push_front(arr, n, value);
+	Print(arr, n);
+
+	arr = pop_back(arr, n);
+	Print(arr, n);
+
+	arr = pop_front(arr, n);
+	Print(arr, n);
+
+	delete[] arr;
 }
 void FillRand(int arr[], const int n)
 {
@@ -21,10 +43,56 @@ void FillRand(int arr[], const int n)
 		*(arr + i) = rand() % 100; //Через арифметику указателей и оператор разыменования
 	}
 }
+
 void Print(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";	   //через оператор индексирования (квадратные скобки)
 	}
+	cout << endl;
+}
+
+int* push_back(int arr[], int& n, const int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	delete[] arr;
+	arr = buffer;
+	buffer = nullptr;
+	arr[n] = value;
+	n++;
+	return arr;
+}
+
+int* push_front(int arr[], int& n, const int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i+1] = arr[i];
+	}
+	delete[] arr;
+	buffer[0] = value;
+	n++;
+	return buffer;
+}
+
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];
+	delete[] arr;
+	return buffer;
+}
+
+int* pop_front(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i+1];
+	delete[] arr;
+	return buffer;
 }
