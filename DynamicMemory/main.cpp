2c39d6c70b7487c9f88b82071fe86e7aff2d67df
ilyav <ilyav@ILIA>
2025-06-11 23:19:15 +0500

    DynamicArrays2 stars

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -1,18 +1,31 @@
 #include<iostream>
 using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
 
-void FillRand(int arr[], const int n);
+void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(int** arr, const int rows, const int cols, int minRand = 0, int maxRand = 100);
+
 void Print(int arr[], const int n);
+void Print(int** arr, const int rows, const int cols);
 
 int* push_back(int arr[], int& n, const int value);
 int* push_front(int arr[], int& n, const int value);
 
+int** push_row_back(int** arr, int& rows, const int cols);
+void push_col_back(int** arr, const int rows, int& cols);
+
 int* pop_back(int arr[], int& n);
 int* pop_front(int arr[], int& n);
 
+
+//#define dynamic_memory_1
+#define dynamic_memory_2
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef dynamic_memory_1
 	int n;
 	cout << "go size array: "; cin >> n;
 	int* arr = new int[n];
@@ -35,12 +48,58 @@ void main()
 	Print(arr, n);
 
 	delete[] arr;
+#endif
+
+#ifdef dynamic_memory_2
+
+	int rows;
+	int cols;
+	cout << "Введите кол - во строк : "; cin >> rows;
+	cout << "Введите кол - во элементов строки : "; cin >> cols;
+	
+	int** arr = new int* [rows];
+
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols];
+	}
+
+	FillRand(arr, rows, cols);	
+	Print(arr, rows, cols);
+	
+	arr = push_row_back(arr, rows, cols);
+	FillRand(arr[rows - 1], cols, 100, 1000);
+	Print(arr, rows, cols);
+
+	push_col_back(arr, rows, cols);
+	for (int i = 0; i < rows; i++)arr[i][cols - 1] = rand() % 1000;
+	Print(arr, rows, cols);
+	
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] arr[i];
+	}
+
+	delete[] arr;
+
+#endif
 }
-void FillRand(int arr[], const int n)
+void FillRand(int arr[], const int n, int minRand, int maxRand)
 {
 	for (int i = 0; i < n; i++)
 	{
-		*(arr + i) = rand() % 100; //Через арифметику указателей и оператор разыменования
+		*(arr + i) = rand() % (maxRand - minRand) + minRand; //Через арифметику указателей и оператор разыменования
+	}
+}
+
+void FillRand(int** arr, const int rows, const int cols, int minRand, int maxRand)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
 	}
 }
 
@@ -49,6 +108,19 @@ void Print(int arr[], const int n)
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";	   //через оператор индексирования (квадратные скобки)
+	}
+	cout << endl;
+}
+
+void Print(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
 	}
 	cout << endl;
 }
@@ -66,6 +138,28 @@ int* push_back(int arr[], int& n, const int value)
 	arr[n] = value;
 	n++;
 	return arr;
+}
+
+int** push_row_back(int** arr, int& rows, const int cols)
+{
+	int** buffer = new int*[rows + 1];
+	for (int i = 0; i < rows; i++)buffer[i] = arr[i];
+	delete[] arr;
+	buffer[rows] = new int[cols]{};
+	rows++;
+	return buffer;
+}
+
+void push_col_back(int** arr, const int rows, int& cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		int* buffer = new int[cols + 1] {};
+		for (int j = 0; j < cols; j++)buffer[j] = arr[i][j];
+		delete[] arr[i];
+		arr[i] = buffer;		
+	}
+	cols++;
 }
 
 int* push_front(int arr[], int& n, const int value)
